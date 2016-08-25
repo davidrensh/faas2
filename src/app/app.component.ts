7e288f7a7ac95e3d564e293b06f2f2730d949bb9
davidrensh <davidrensh@hotmail.com>
2016-08-24 22:11:51 -0600

    01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: 'app.component.html',
+//   styleUrls: ['app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app works!';
+// }
+
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
+  //moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works2222!';
+  items: FirebaseListObservable<any[]>;
+  constructor(af: AngularFire) {//
+    console.log("aaaa");
+    this.items = af.database.list('items');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,37 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule, ApplicationRef } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     CommonModule,
+//     FormsModule
+//   ],
+//   providers: [],
+//   entryComponents: [AppComponent],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule {
+
+// }
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+export const firebaseConfig = {
+  apiKey: "AIzaSyDUKlFAFOci3eIKn84YGr4Z1A4fexfyfNg",
+  authDomain: "formaas.firebaseapp.com",
+  databaseURL: "https://formaas.firebaseio.com",
+  storageBucket: "gs://firebase-formaas.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -11,7 +40,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule, AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   entryComponents: [AppComponent],
@@ -20,3 +49,28 @@ import { AppComponent } from './app.component';
 export class AppModule {
 
 }
+
+// import * as firebase from 'firebase';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableProdMode, NgModule } from '@angular/core';
+// import { AppComponent } from './app.component';
+// import { AngularFireModule } from 'angularfire2';
+
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyDUKlFAFOci3eIKn84YGr4Z1A4fexfyfNg",
+//   authDomain: "formaas.firebaseapp.com",
+//   databaseURL: "https://formaas.firebaseio.com",
+//   storageBucket: "gs://firebase-formaas.appspot.com"
+// };
+
+// @NgModule({
+//   imports: [
+//     BrowserModule
+//     , AngularFireModule.initializeApp(firebaseConfig)
+//   ],
+//   declarations: [AppComponent],
+//   bootstrap: [ AppComponent ]
+// })
+  
+// export class AppModule {}
