2aa89e33dca2a8628ae2ef53fd1615004c1dd1cc
davidrensh <davidrensh@hotmail.com>
2016-09-16 14:12:45 -0600

    add editor save form

--- src/app/tempeditor/tempeditor.component.ts
+++ src/app/tempeditor/tempeditor.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit, ElementRef, ViewChild, NgZone, Input } from '@angular/core';
-
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-tempeditor',
-  template: `
-    <ckeditor [(ngModel)]="content" debounce="500"  >
-      <p>Hello <strong>world</strong></p>
-    </ckeditor>
-    <div [innerHTML]="content"></div>
-  `,
+  templateUrl: 'Tempeditor.component.html',
   styleUrls: ['tempeditor.component.css']
 })
 export class TempeditorComponent implements OnInit {
   content: any;
-  constructor(private _zone: NgZone) {
-    console.log("testtttt11111");
-    this.content = `<p>My HTML22</p>`;
-   }
+  //html: string;
+  constructor(private _zone: NgZone, public af: AngularFire) {
+    // console.log("testtttt11111");
+    // this.content = `<p>My HTML22</p>`;
+  }
 
   ngOnInit() {
     //    CKEditor..replace( targetId );
   }
-  // afterAll() {
-
-  // }
+  saveForm() {
+    let formname: string = "f01";
+    console.log("html=" + this.content);
+    this.af.database.object("/forms/" + formname).update({
+      name: formname,
+      contenthtml: this.content,
+      updateddate: (new Date()).toISOString().substr(0, 10)
+    });
+  }
 }
